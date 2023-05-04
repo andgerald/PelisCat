1d9fc383ca5ccbe81571fc4b04f41beb4009a543
andgeral <geraldinebecerra93@gmal.com>
2023-05-03 23:22:56 -0400

    se agrega sass

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/home/Home';
+import { MovieDetails } from './components/movieDetails/MovieDetails';
 
 const Main = () => {
 return (         
     <Routes>
         <Route path='/home' element={<Home/>} />
+        <Route path='/movie' element={<MovieDetails/>} />
+
     </Routes>
 );
 }
---
+++ src/components/movieDetails/MovieDetails.tsx
@@ -0,0 +1,13 @@
+import '../../styles/components/MovieDetails.css';
+import { Button } from 'react-bootstrap';
+export const MovieDetails = () => {
+    return (
+        <>
+        <div className="backgroundMovie">
+        <Button type="button" variant="primary">Primary</Button>
+        <p>hola</p>
+      </div>
+        </>
+      
+    )
+  }
